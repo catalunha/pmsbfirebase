8b4524fa3cd29f54f75d66249ec5c605b585fe4c
catalunha <catalunha.mj@gmail.com>
2020-03-28 07:29:50 -0300

    apenas conexão no ubuntu 19.10 do prof

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,21 @@
 import * as functions from 'firebase-functions';
+// import admin = require('firebase-admin');
+
+
+// // Take the text parameter passed to this HTTP endpoint and insert it into the
+// // Realtime Database under the path /messages/:pushId/original
+// exports.addMessage = functions.https.onRequest(async (req, res) => {
+//     // Grab the text parameter.
+//     const original = req.query.text;
+//     // Push the new message into the Realtime Database using the Firebase Admin SDK.
+//     const snapshot = await admin.database().ref('/messages').push({original: original});
+//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//     res.redirect(303, snapshot.ref.toString());
+//   });
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase! Deu certooooo...");
+});
 
 
 // /**
@@ -15,9 +32,9 @@ import * as functions from 'firebase-functions';
  * IA
  */
 
-import * as IaItemRespostaFunction from './funcoes/IA_item_resposta_functions';
+// import * as IaItemRespostaFunction from './funcoes/IA_item_resposta_functions';
 
-exports.IaItemRespostaOnUpdateFunction = functions.firestore.document('IAItens/{iAItensId}/IAItemResposta/{iAItemRespostaId}').onUpdate(IaItemRespostaFunction.iniciarIaItemRespostaUpdate);
+// exports.IaItemRespostaOnUpdateFunction = functions.firestore.document('IAItens/{iAItensId}/IAItemResposta/{iAItemRespostaId}').onUpdate(IaItemRespostaFunction.iniciarIaItemRespostaUpdate);
 
 
 
